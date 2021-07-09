1f553f07f440cbaf2990400e62f10f5ee46a2c6d
heebok <lehebk@naver.com>
2021-07-09 16:56:42 +0900

    each

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -92,6 +92,22 @@ public class BasicController {
         return "basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model){
+        addUser(model);
+        return "/basic/each";
+    }
+
+    private void addUser(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+        list.add(new User("UserD", 40));
+
+        model.addAttribute("users", list);
+    }
+
 
 
     @Data
